e46b195f7cab1eda0a905dd87ae4dfaf46cf43df
Joseph Slone <joe.slone@gmail.com>
2015-06-12 12:36:22 -0400

    Added some initial comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# Programming Notes:
+#
+#   (FYI - I put lots of comments in my code, mostly for my benefit)
+#
+#   < From the R manual >
+#   The operators <<- and ->> are normally only used in functions,
+#   and cause a search to made through parent environments for an
+#   existing definition of the variable being assigned. If such a
+#   variable is found (and its binding is not locked) then its value
+#   is redefined, otherwise assignment takes place in the global
+#   environment.
+
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
